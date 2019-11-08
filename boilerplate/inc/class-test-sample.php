1ff057d78c14bfbc25904c7f82e88e554a07d72b
Robert O'Rourke <robert@o-rourke.org>
2019-11-08 16:18:20 +0000

    move sample test to inc

---
+++ boilerplate/inc/class-test-sample.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Sample Unit Test Class.
+ *
+ * Edit or delete this file to add your own tests.
+ */
+
+namespace Project\Tests;
+
+use WP_UnitTestCase;
+
+class Test_Sample extends WP_UnitTestCase {
+
+	public function test_it_works() {
+		$this->assertTrue( function_exists( 'do_action' ) );
+	}
+
+}
