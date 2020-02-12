4d5f14b842e86dd0e6db38bb59a36ba7723db000
dashaluna <dashaluna@gmail.com>
2020-02-12 16:27:08 +0000

    Remove caption so table looks full width

--- inc/altis_config/class-qm-output-html-altis-config.php
+++ inc/altis_config/class-qm-output-html-altis-config.php
@@ -29,9 +29,6 @@ class QM_Output_Html_Altis_Config extends QM_Output_Html {
 
 		<?php $this->before_tabular_output(); ?>
 
-		<caption>
-			<h2><?php echo esc_html( $this->name() ); ?></h2>
-		</caption>
 		<thead>
 		<tr>
 			<th><?php echo esc_html__( 'Module', 'altis' ); ?></th>
