3a8117aa2502613a31951d4cb1d1a5d49b8ab35f
dashaluna <dashaluna@gmail.com>
2020-02-13 16:07:27 +0000

    Rename ElasticPress QM menu title

--- inc/namespace.php
+++ inc/namespace.php
@@ -40,6 +40,10 @@ function on_plugins_loaded() {
 
 	// Register outputter.
 	add_filter( 'qm/outputter/html', __NAMESPACE__ . '\\register_altis_config_qm_output_html' );
+
+	// Rename menu title for "Debug Bar: ElasticPress" into "ElasticPress".
+	// Hook on 300, as QM_Output_Html_Debug_Bar adds menus on 200.
+	add_filter( 'qm/output/menus', __NAMESPACE__ . '\\rename_elasticpress_qm_menu_title', 300 );
 }
 
 /**
@@ -67,4 +71,36 @@ function register_altis_config_qm_output_html( array $output ) : array {
 		$output['altis-config'] = new Altis_Config\QM_Output_Html_Altis_Config( $collector );
 	}
 	return $output;
+}
+
+/**
+ * Rename menu title shown in Query Monitor's admin toolbar menu
+ * from "Debug Bar: ElasticPress" into "ElasticPress".
+ *
+ * @param array $menus Array of menus for Query Monitor.
+ *
+ * @return array Array of menus for Query Monitor with altered menu name item.
+ */
+function rename_elasticpress_qm_menu_title( array $menus ) : array {
+
+	// Query Monitor sets this to "debug_bar_{$class_name_strtolower}".
+	$debug_bar_id = 'debug_bar_ep_debug_bar_elasticpress';
+
+	$menu_item = $menus[ "qm-{$debug_bar_id}" ] ?? [];
+	if ( ! $menu_item ) {
+		return $menus;
+	}
+
+	// Get QM Collector, so we can access ElasticPress panel and reuse its title,
+	// keeping its translations if any.
+	$collector = QM_Collectors::get( $debug_bar_id );
+	if ( ! $collector ) {
+		return $menus;
+	}
+
+	// Overwrite the title.
+	$title = $collector->get_panel()->title();
+	$menus[ "qm-{$debug_bar_id}" ]['title'] = $title;
+
+	return $menus;
 }
