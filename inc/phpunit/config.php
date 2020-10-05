343611bddaaabd56bc07e952be06bfa7857568f7
Robert O'Rourke <robert@o-rourke.org>
2020-10-05 11:42:08 +0000

    Move cache key salt to config file

--- inc/phpunit/config.php
+++ inc/phpunit/config.php
@@ -16,6 +16,9 @@ defined( 'WP_TESTS_DOMAIN' ) or define( 'WP_TESTS_DOMAIN', 'example.org' );
 defined( 'WP_TESTS_EMAIL' ) or define( 'WP_TESTS_EMAIL', 'admin@example.org' );
 defined( 'WP_TESTS_TITLE' ) or define( 'WP_TESTS_TITLE', 'Test Blog' );
 
+// Ensure cache key salt is different for test runs.
+defined( 'WP_CACHE_KEY_SALT' ) or define( 'WP_CACHE_KEY_SALT', 'phpunit' );
+
 // Ensure tests use their own ElasticPress indexes.
 defined( 'EP_INDEX_PREFIX' ) or define( 'EP_INDEX_PREFIX', 'tests_' );
 
