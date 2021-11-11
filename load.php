63600981c59376ddc1cdb635e1556161a893c03a
Shady Sharaf <shady@sharaf.me>
2021-11-11 16:17:45 +0200

    Move test constant definitions to dev-tools

--- load.php
+++ load.php
@@ -29,3 +29,20 @@ add_action( 'altis.modules.init', function () {
 		__NAMESPACE__ . '\\bootstrap'
 	);
 } );
+
+// Testing constants to switch to test db/redis/elastic
+add_action( 'altis.modules.init', function() {
+	// Detect requests from Codeception/phpunit, primarely to use the test database.
+	$is_test_request = (
+		isset( $_SERVER['HTTP_X_TEST_REQUEST'] )
+		|| ( isset( $_SERVER['HTTP_USER_AGENT'] ) && strpos( $_SERVER['HTTP_USER_AGENT'], 'wp-browser' ) !== false )
+		|| getenv( 'WPBROWSER_HOST_REQUEST' )
+	);
+
+	// For acceptance tests, change DB name and Elastic/Redis prefixes.
+	if ( $is_test_request ) {
+		define( 'DB_NAME', 'test' );
+		define( 'EP_INDEX_PREFIX', 'tests_' );
+		define( 'WP_CACHE_KEY_SALT', 'codecept' );
+	}
+}, 1 );
