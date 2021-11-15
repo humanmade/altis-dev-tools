0b3783be5f37193573ae6aa805685579410e5169
Shady Sharaf <shady@sharaf.me>
2021-11-15 20:30:26 +0200

    :white_check_mark: Use absolute path for the load.php location

--- tests/_support/AcceptanceTester.php
+++ tests/_support/AcceptanceTester.php
@@ -36,7 +36,7 @@ class AcceptanceTester extends \Codeception\Actor
 	 * @return void
 	 */
 	public function preLoadConfig( string $file, callable $function, bool $rollback_on_shutdown = true ) {
-		$path = 'tests/load.php';
+		$path = __DIR__ . '/../../../../tests/load.php';
 
 		if ( ! file_exists( $file ) ) {
 			throw new Exception( 'File path does not exist.' );
