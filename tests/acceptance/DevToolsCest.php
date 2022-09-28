56af1db7a318baf0855b8a9ef24382fadfc5f680
Robin Devitt <robin@humanmade.com>
2022-09-28 13:25:37 +0200

    Ensure Dev tools is activate before running the test.

--- tests/acceptance/DevToolsCest.php
+++ tests/acceptance/DevToolsCest.php
@@ -12,6 +12,35 @@ use Codeception\Util\Locator;
 class DevToolsCest {
 
 	/**
+	 * Rollback callback for the Dev Tools activation bootstrap call.
+	 *
+	 * @var callable
+	 */
+	protected $rollback = null;
+
+	/**
+	 * Make sure Dev Tools is activated.
+	 *
+	 * @param AcceptanceTester $I Actor object.
+	 *
+	 * @return void
+	 */
+	public function _before( AcceptanceTester $I ) {
+		$this->rollback = $I->bootstrapWith( [ __CLASS__, '_enableDevTools' ] );
+	}
+
+	/**
+	 * Deactivate Dev Tools after tests are finished.
+	 *
+	 * @param AcceptanceTester $I Actor object.
+	 *
+	 * @return void
+	 */
+	public function _after( AcceptanceTester $I ) {
+		call_user_func( $this->rollback );
+	}
+
+	/**
 	 * Open Query Monitor / Dev Tools panel and check all tabs are working.
 	 *
 	 * @param AcceptanceTester $I Tester
@@ -40,6 +69,18 @@ class DevToolsCest {
 		// Test and confrim AWS X-Ray tab and content renders, using the "Segment Name" column heading.
 		$I->click( Locator::find( 'button', ['data-qm-href' => '#qm-aws-xray'] ) );
 		$I->see( 'Segment Name', 'th' );
+	}
+
+	/**
+	 * Activate Dev Tools.
+	 *
+	 * @return void
+	 */
+	public static function _enableDevTools() {
+		add_filter( 'altis.config', function( $config ) {
+			$config['modules']['dev-tools']['enabled'] = true;
+			return $config;
+		} );
 	}
 
 }
